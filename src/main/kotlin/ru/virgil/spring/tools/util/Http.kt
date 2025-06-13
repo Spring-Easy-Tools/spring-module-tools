4240b08f6b9715078c0604b615032cd42b98aca0
ViRGiL175 <virgil7g@gmail.com>
2025-06-13 15:23:42 +0400

    refactor: update orNotFound and orBadRequest methods to use getOrNull for safer optional handling

--- src/main/kotlin/ru/virgil/spring/tools/util/Http.kt
+++ src/main/kotlin/ru/virgil/spring/tools/util/Http.kt
@@ -7,6 +7,7 @@ import org.springframework.web.context.request.RequestContextHolder
 import org.springframework.web.context.request.ServletRequestAttributes
 import org.springframework.web.server.ResponseStatusException
 import java.util.*
+import kotlin.jvm.optionals.getOrNull
 
 @Suppress("MemberVisibilityCanBePrivate", "unused")
 object Http {
@@ -33,7 +34,7 @@ object Http {
         return ResponseStatusException(NOT_FOUND, message, cause)
     }
 
-    inline fun <reified T> Optional<T>.orNotFound(comment: Any? = null): T = this.get().orNotFound(comment)
+    inline fun <reified T : Any> Optional<T>.orNotFound(comment: Any? = null): T = this.getOrNull().orNotFound(comment)
 
     fun <T> T?.orNotFound(clazz: Class<T>, comment: Any? = null): T {
         val message = when (comment) {
@@ -51,7 +52,7 @@ object Http {
         return this ?: throw ResponseStatusException(NOT_FOUND, message)
     }
 
-    inline fun <reified T> Optional<T>.orBadRequest(comment: Any? = null): T = this.get().orBadRequest(comment)
+    inline fun <reified T : Any> Optional<T>.orBadRequest(comment: Any? = null): T = this.getOrNull().orBadRequest(comment)
 
     inline fun <reified T> T?.orBadRequest(comment: Any? = null): T {
         val message = when (comment) {
@@ -61,7 +62,7 @@ object Http {
         return this ?: throw ResponseStatusException(HttpStatus.BAD_REQUEST, message)
     }
 
-    inline fun <reified T> Optional<T>.thenConflict(comment: Any? = null) = this.get().thenConflict(comment)
+    inline fun <reified T : Any> Optional<T>.thenConflict(comment: Any? = null) = this.getOrNull().thenConflict(comment)
 
     inline fun <reified T> T?.thenConflict(comment: Any? = null) {
         val message = when (comment) {
