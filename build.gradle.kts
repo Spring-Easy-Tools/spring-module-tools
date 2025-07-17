15671cccb92ce5c0146acb35b8eb3c69a5900d94
EsQLap <vip.ezkatka@gmail.com>
2025-07-17 10:31:42 +0400

    Add tika dependency for .doc and .docx extensions

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,8 +47,9 @@ dependencies {
     implementation("com.sksamuel.scrimage:scrimage-core:4.3.0")
     api("io.exoquery:pprint-kotlin:2.0.2")
     api("com.google.truth:truth:1.1.3")
-    api("org.apache.tika:tika-core:2.7.0")
-    api("org.apache.tika:tika-parsers:2.7.0")
+    api("org.apache.tika:tika-core:3.2.1")
+    api("org.apache.tika:tika-parsers:3.2.1")
+    api("org.apache.tika:tika-parsers-standard-package:3.2.1")
     api("io.kotest:kotest-assertions-core:5.6.1")
     testApi("io.kotest:kotest-assertions-core:5.6.1")
 }
