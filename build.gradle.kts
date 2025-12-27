5e6b0619e58b6be6088c64be028493ca57dc45d8
ViRGiL175 <virgil7g@gmail.com>
2025-12-28 03:40:57 +0400

    fix: revert kotlinVersion to 2.2.21 for compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("org.springframework.boot") version "4.0.1"
     id("io.spring.dependency-management") version "1.1.7"
     // CodeQL currently supports versions below 2.2.30
-    val kotlinVersion = "2.3.0"
+    val kotlinVersion = "2.2.21"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("plugin.jpa") version kotlinVersion
